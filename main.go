ea7b6c324c9f4783989c69f320963aaa48fef9bb
Oscar Maldonado <oscarmaldonado@Oscars-MacBook-Pro.local>
2021-11-02 22:25:29 -0700

    first commit:

---
+++ main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"net/http"
+	"fmt"
+	"log"
+)
+
+var port string = ":5005"
+
+var emails = [2]string{"oscarmmgg1234@gmail.com","edwardcordero@gmail.com"}
+var passwords = [2]string{"oscarTester23","edwardTester25"}
+
+
+func credentialValidation(username string, password string) bool { 
+var result bool = false
+if username != "" {
+	for i := 0; i < len(emails); i++ {
+		if username == emails[i] {
+			for i := 0; i < len(passwords); i++ {
+				if password == passwords[i]{
+					result = true
+				}
+			}
+		}
+	}
+	return result
+} else{
+	return result
+}
+
+}
+
+func userValidation(w http.ResponseWriter, r* http.Request){
+rUsername := r.Header.Get("username")
+rPassword := r.Header.Get("password")
+result := credentialValidation(rUsername, rPassword)
+if result == true {
+	fmt.Fprintf(w, "{valid: true}")
+} else {
+	fmt.Fprintf(w, "{valid: false}")
+}
+
+
+}
+
+func handleRequest(){
+	http.HandleFunc("/auth", userValidation)
+}
+
+
+
+
+func main(){
+	
+	handleRequest()
+	log.Fatal(http.ListenAndServe(port, nil))
+
+}
